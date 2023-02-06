fab56388362281bdabc851f4ca7b3398bee90c26
langsamu <langsamu@users.noreply.github.com>
2023-02-06 17:15:01 +0000

    deploy: bfb02bfdf4baabab505501f5023d710b3163b3ae

---
+++ 0.1.4-SNAPSHOT/test/base/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.inrupt.rdf.wrapping.test.base","l":"HasSameMethods"},{"p":"com.inrupt.rdf.wrapping.test.base","l":"ObjectSetBase"}]
