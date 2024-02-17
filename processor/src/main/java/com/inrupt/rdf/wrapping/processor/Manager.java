d578953c076708b8cb64a80b0138c693463fd48f
Samu Lang <samu.lang@inrupt.com>
2024-02-17 19:29:46 +0000

    Manager readability and coverage

--- processor/src/main/java/com/inrupt/rdf/wrapping/processor/Manager.java
+++ processor/src/main/java/com/inrupt/rdf/wrapping/processor/Manager.java
@@ -37,44 +37,76 @@ public final class Manager {
     private Manager() {
     }
 
-    public static <T> T wrap(final Model original, final Class<T> interfaceType) {
-        return wrap(original, interfaceType, Model.class);
+    public static <T> T wrap(final Model original, final Class<T> definition) {
+        return wrap(original, definition, Model.class);
     }
 
-    public static <T> T wrap(final Dataset original, final Class<T> interfaceType) {
-        return wrap(original, interfaceType, Dataset.class);
+    public static <T> T wrap(final Dataset original, final Class<T> definition) {
+        return wrap(original, definition, Dataset.class);
     }
 
-    private static <T> T wrap(final Object original, final Class<T> interfaceType, final Class<?> parameterType) {
-        final ClassLoader classLoader = interfaceType.getClassLoader();
-        final String implTypeName = asImplementation(interfaceType.getName());
+    private static <T> T wrap(final Object original, final Class<T> definition, final Class<?> parameterType) {
+        final Class<?> rawImplementation = findImplementation(definition);
+        final Class<? extends T> implementation = subclass(rawImplementation, definition);
+        final Method wrap = findWrapMethod(implementation, parameterType);
+        ensureNotVoid(wrap);
+        final Object result = invoke(wrap, original);
+        return cast(result, definition);
+    }
 
-        final Class<? extends T> implClass;
+    private static <T> Class<?> findImplementation(final Class<T> definition) {
+        final String implementation = asImplementation(definition.getName());
+        final ClassLoader loader = definition.getClassLoader();
+
         try {
-            implClass = Class.forName(implTypeName, true, classLoader).asSubclass(interfaceType);
+            return Class.forName(implementation, true, loader);
+
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("implementation not found", e);
+        }
+    }
+
+    private static <T> Class<? extends T> subclass(final Class<?> implementation, final Class<T> definition) {
+        try {
+            return implementation.asSubclass(definition);
+
         } catch (ClassCastException e) {
-            // TODO: probably not needed as it's generated, match on $impl
             throw new RuntimeException("implementation type mismatch", e);
         }
+    }
 
-
-        final Method wrapMethod;
+    private static <T> Method findWrapMethod(final Class<? extends T> implementation, final Class<?> parameterType) {
         try {
-            wrapMethod = implClass.getMethod(WRAP, parameterType);
+            return implementation.getDeclaredMethod(WRAP, parameterType);
+
         } catch (NoSuchMethodException e) {
             throw new RuntimeException("wrap method not found", e);
         }
+    }
 
+    private static void ensureNotVoid(final Method wrap) {
+        if (wrap.getReturnType() == void.class) {
+            throw new RuntimeException("wrap method is void");
+        }
+    }
+
+    private static Object invoke(final Method wrap, final Object... original) {
         try {
-            return interfaceType.cast(wrapMethod.invoke(null, original));
+            return wrap.invoke(null, original);
+
         } catch (IllegalAccessException e) {
             throw new RuntimeException("wrap method inaccessible", e);
+
         } catch (InvocationTargetException e) {
             throw new RuntimeException("wrap method threw exception", e);
+        }
+    }
+
+    private static <T> T cast(final Object result, final Class<T> definition) {
+        try {
+            return definition.cast(result);
+
         } catch (ClassCastException e) {
-            // TODO: probably not needed as it's generated, match on $impl
             throw new RuntimeException("wrap method return type mismatch", e);
         }
     }
---
+++ processor/src/test/java/com/inrupt/rdf/wrapping/processor/ManagerTest.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright Inrupt Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.inrupt.rdf.wrapping.processor;
+
+import static com.inrupt.rdf.wrapping.processor.Manager.wrap;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import java.util.stream.Stream;
+
+import org.apache.jena.query.Dataset;
+import org.apache.jena.query.DatasetFactory;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+@DisplayName("Manager")
+class ManagerTest {
+    @DisplayName("fails to wrap with error")
+    @ParameterizedTest(name = "[{1}] when definition is [{0}]")
+    @MethodSource
+    void wrapFails(final Class<?> definition, final String error) {
+        final Model model = ModelFactory.createDefaultModel();
+
+        final Throwable t = assertThrows(RuntimeException.class, () -> wrap(model, definition));
+        assertThat(t, hasProperty("message", is(error)));
+    }
+
+    static Stream<Arguments> wrapFails() {
+        return Stream.of(
+                arguments(NoImplementation.class, "implementation not found"),
+                arguments(ImplementationTypeMismatch.class, "implementation type mismatch"),
+                arguments(WrapMissing.class, "wrap method not found"),
+                arguments(WrapVoid.class, "wrap method is void"),
+                arguments(WrapInaccessible.class, "wrap method inaccessible"),
+                arguments(WrapThrows.class, "wrap method threw exception"),
+                arguments(WrapMismatch.class, "wrap method return type mismatch")
+        );
+    }
+
+    @Test
+    @DisplayName("wraps graph")
+    void wrapModel() {
+        final Model model = ModelFactory.createDefaultModel();
+
+        assertDoesNotThrow(() -> wrap(model, OkModel.class));
+    }
+
+    @Test
+    @DisplayName("wraps dataset")
+    void wrapDataset() {
+        final Dataset dataset = DatasetFactory.create();
+
+        assertDoesNotThrow(() -> wrap(dataset, OkDataset.class));
+    }
+}
+
+interface NoImplementation {
+}
+
+interface ImplementationTypeMismatch {
+}
+
+class ImplementationTypeMismatch_$impl {
+}
+
+interface WrapMissing {
+}
+
+class WrapMissing_$impl implements WrapMissing {
+}
+
+interface WrapVoid {
+}
+
+@SuppressWarnings("HideUtilityClassConstructor") // Not really a utility class
+class WrapVoid_$impl implements WrapVoid {
+    public static void wrap(final Model ignored) {
+    }
+}
+
+interface WrapInaccessible {
+}
+
+class WrapInaccessible_$impl implements WrapInaccessible {
+    private static WrapInaccessible wrap(final Model ignored) {
+        return null;
+    }
+}
+
+interface WrapThrows {
+}
+
+@SuppressWarnings("HideUtilityClassConstructor") // Not really a utility class
+class WrapThrows_$impl implements WrapThrows {
+    public static Object wrap(final Model ignored) {
+        throw new RuntimeException();
+    }
+}
+
+interface WrapMismatch {
+}
+
+@SuppressWarnings("HideUtilityClassConstructor") // Not really a utility class
+class WrapMismatch_$impl implements WrapMismatch {
+    public static Object wrap(final Model ignored) {
+        return new Object();
+    }
+}
+
+interface OkModel {
+}
+
+@SuppressWarnings("HideUtilityClassConstructor") // Not really a utility class
+class OkModel_$impl implements OkModel {
+    public static OkModel wrap(final Model ignored) {
+        return new OkModel() {
+        };
+    }
+}
+
+interface OkDataset {
+}
+
+@SuppressWarnings("HideUtilityClassConstructor") // Not really a utility class
+class OkDataset_$impl implements OkDataset {
+    public static OkDataset wrap(final Dataset ignored) {
+        return new OkDataset() {
+        };
+    }
+}
